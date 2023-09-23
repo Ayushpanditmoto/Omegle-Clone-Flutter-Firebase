62eb2a23c5450a5855df6b6bbe895e11acf5c533
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-09-23 22:13:32 +0530

    feat: basic packages done :tada:

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_database
+import flutter_webrtc
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
